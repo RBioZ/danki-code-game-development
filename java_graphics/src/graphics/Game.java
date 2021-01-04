05f88ea5fd9625abe61fa98132a25387e83d5220
RBioZ <jonatha_rihan@hotmail.com>
2021-01-04 00:19:42 -0300

    AULA #02 - Game Loop

--- java_graphics/src/graphics/Game.java
+++ java_graphics/src/graphics/Game.java
@@ -8,12 +8,18 @@ import javax.swing.JFrame;
 public class Game extends Canvas implements Runnable {
 
 	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
 	private final int WIDTH = 160;
 	private final int HEIGHT = 120;
 	private final int SCALE = 3;
 	
 	public Game() {
-		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+	}
+	
+	public void initFrame() {
 		frame = new JFrame("Game #01");
 		frame.add(this);
 		frame.setResizable(false);
@@ -21,17 +27,60 @@ public class Game extends Canvas implements Runnable {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
 		
 	}
 	
 	public static void main(String[] args) {
 		Game game = new Game();
+		game.start();
 
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames ++;
+				delta --;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+			
+		}
 		
 	}
 
