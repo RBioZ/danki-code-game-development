0c52af52c1691f4bc3abe987e5193832e1b56fa8
RBioZ <jonatha_rihan@hotmail.com>
2021-01-19 21:42:56 -0300

    :sparkles: Pong II-I

---
+++ Pong/src/pong/Player.java
@@ -0,0 +1,16 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player {
+
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(200, 110, 40, 10);
+	}
+}
