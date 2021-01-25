eb252a6b541720aa507ab9785f2b0eac375bca25
RBioZ <jonatha_rihan@hotmail.com>
2021-01-24 23:48:59 -0300

    :sparkles: Fist Commit Game #01

---
+++ Game_01/src/com/rbioz/entities/Entity.java
@@ -0,0 +1,70 @@
+package com.rbioz.entities;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Entity {
+	
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+	private BufferedImage sprite;
+	
+	public Entity(int x, int y, int width, int height, BufferedImage sprite) {
+		this.setX(x);
+		this.setY(y);
+		this.setWidth(width);
+		this.setHeight(height);
+		this.setSprite(sprite);
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public BufferedImage getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(BufferedImage sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void tick (){
+			
+	}
+	
+	public void render(Graphics g) {
+		g.drawImage(sprite, this.getX(), this.getY(), null);
+	}
+}
