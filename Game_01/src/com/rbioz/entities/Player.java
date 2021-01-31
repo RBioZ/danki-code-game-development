c1c85905bdd88ec7af517b8798e6ef703c212fbc
RBioZ <jonatha_rihan@hotmail.com>
2021-01-31 19:58:40 -0300

    :sparkles: Camera Performace

--- Game_01/src/com/rbioz/entities/Player.java
+++ Game_01/src/com/rbioz/entities/Player.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 
 import com.rbioz.main.Game;
 import com.rbioz.world.Camera;
+import com.rbioz.world.World;
 
 public class Player extends Entity{
 	
@@ -73,8 +74,8 @@ public class Player extends Entity{
 			index = 0;
 		}
 		
-		Camera.x = this.getX() - (Game.WIDTH/2);
-		Camera.y = this.getY() - (Game.HEIGHT/2);
+		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH/2),0,World.WIDTH * 16 - Game.WIDTH);
+		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT/2),0,World.HEIGHT * 16 - Game.HEIGHT);
 	}
 	
 	public void render(Graphics g) {
