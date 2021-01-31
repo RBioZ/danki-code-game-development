b6573903c39dc07dc906cfa366570b009ff4dd40
RBioZ <jonatha_rihan@hotmail.com>
2021-01-31 10:03:29 -0300

    :sparkles: Moves

--- Game_01/src/com/rbioz/main/Game.java
+++ Game_01/src/com/rbioz/main/Game.java
@@ -3,21 +3,19 @@ package com.rbioz.main;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-// import java.awt.Font;
 import java.awt.Graphics;
-// import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JFrame;
-
 import com.rbioz.entities.Entity;
 import com.rbioz.entities.Player;
 import com.rbioz.graphics.Spritesheet;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
 	private static final long serialVersionUID = 1L;
 	public static JFrame frame;
@@ -32,14 +30,16 @@ public class Game extends Canvas implements Runnable {
 	public List<Entity> entities;
 	public Spritesheet spritesheet;
 	
+	private Player player;
+	
 	public Game() {
+		addKeyListener(this);
 		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		initFrame();
 		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
 		entities = new ArrayList<Entity>();
 		spritesheet = new Spritesheet("/spritesheet.png");
-		
-		Player player = new Player(0,0,16,16, spritesheet.getSprite(32,0,16,16));
+		player = new Player(0,0,16,16, spritesheet.getSprite(32,0,16,16));
 		entities.add(player);
 	}
 	
@@ -135,6 +135,55 @@ public class Game extends Canvas implements Runnable {
 		}
 		
 		stop();
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D ) {
+			System.out.println("RIGHT");
+			player.right = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A ) {
+			System.out.println("LEFT");
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W ) {
+			System.out.println("UP");
+			player.up = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S ) {
+			System.out.println("DOWN");
+			player.down = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D ) {
+			System.out.println("RIGHT");
+			player.right = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A ) {
+			System.out.println("LEFT");
+			player.left = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W ) {
+			System.out.println("UP");
+			player.up = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S ) {
+			System.out.println("DOWN");
+			player.down = false;
+		}
 		
 	}
 
